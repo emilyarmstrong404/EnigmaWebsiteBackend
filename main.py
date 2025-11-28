4dce8ebc9675a13a4d081982af3aef05fd3a0bf4
Emily Armstrong <emarmstrong2@gmail.com>
2025-11-28 22:27:23 +0000

    origin fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_origins=["https://enigmawebsiteappvercel.vercel.app"],  # Next.js dev server
     allow_methods=["*"],
     allow_headers=["*"],
 )
