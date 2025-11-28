ce974fd43c61b8b86e1bab7383ea04bc8c9bfcab
Emily Armstrong <emarmstrong2@gmail.com>
2025-11-28 23:46:01 +0000

    *

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://enigmawebsiteappvercel.vercel.app"],
+    allow_origins=["*"], #"https://enigmawebsiteappvercel.vercel.app"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
